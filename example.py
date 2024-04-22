458bdbdd7fcfd21d75e46da62e43c28ef62ecba7
Monnapse <masonshuber@gmail.com>
2024-04-22 13:44:21 -0600

    update

--- example.py
+++ example.py
@@ -1,9 +1,16 @@
 import pycom
 
 @pycom.command
-def test(msg1, msg2):
-    print(f"msg1: {msg1}")
-    print(f"msg2: {msg2}")
+def test(msg1, msg2, uppercase=False, lowercase=False):
+    if uppercase:
+        print(f"msg1: {str.upper(msg1)}")
+        print(f"msg2: {str.upper(msg2)}")
+    if lowercase:
+        print(f"msg1: {str.lower(msg1)}")
+        print(f"msg2: {str.lower(msg2)}")
+    else:
+        print(f"msg1: {msg1}")
+        print(f"msg2: {msg2}")
 
 if __name__ == "__main__":
     pycom.start()
--- pycom.py
+++ pycom.py
@@ -3,7 +3,7 @@
     Made by Monnapse
     Create terminal commands the easiest way.
 
-    0.1.0
+    0.2.0
 """
 
 import sys
@@ -16,9 +16,19 @@ def command(function):
 def r_cmd(args):
     args.pop(0)
     function_name = args.pop(0)
+
+    arguments = []
+    long_options = []
+
+    for arg in args:
+        if str.startswith(arg, "--"):
+            long_options.append(arg)
+        else:
+            arguments.append(arg)
+
     for f in commands:
         if f.__name__ == function_name:
-            f(*tuple(args))
+            f(*tuple(arguments))
             break
 
 def start():
